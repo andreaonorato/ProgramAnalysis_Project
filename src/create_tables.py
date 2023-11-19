86090a9032bb2ff0532986a88cc780190e09da5a
schollenkopf <nelson.becker@web.de>
2023-11-19 18:29:46 +0100

    first table

--- src/create_tables.py
+++ src/create_tables.py
@@ -3,7 +3,8 @@
 import time
 import z3
 from concolic import Concolic
-
+import statistics
+import csv
 from utils import find_method
 
 
@@ -13,22 +14,32 @@ test_data = [
         ("../data/example_analysis.json", "calculateEfficiency"),
         [("__ge__", z3.IntVal(0))],
     ),
-    (("../data/example_NoOutOfRange.json", "ShowBalance"), [("__ne__", z3.IntVal(0))]),
+    # (("../data/example_NoOutOfRange.json", "ShowBalance"), [("__ne__", z3.IntVal(0))]),
+    (
+        ("../data/longexample_outofrange.json", "ShowBalance"),
+        [("__ne__", z3.IntVal(0))],
+    ),
 ]
+
 
 for (path, method), limits in test_data:
     c = Concolic(find_method(path, method))
     for skip_loops in [True, False]:
         for i in range(100):
+            execution_time = []
             start_time = time.time()
-            out_of_range, inputs = c.run(limits, skip_loops=skip_loops, k=200)
+            out_of_range, inputs = c.run(limits, skip_loops, k=20000)
+
             run_time = time.time() - start_time
+            if not out_of_range:
+                print("FAIL")
+                break
+            execution_time.append(run_time)
+
             print(run_time, out_of_range, inputs)
 
-
-# we want an output greater than 0, so __ge__
-# c = Concolic(find_method("../data/example_analysis.json", "calculateEfficiency"))
-# c.run([("__ge__", z3.IntVal(0)), ("__lt__", z3.IntVal(100))], False)
-
-# c = Concolic(find_method("../data/example_NoOutOfRange.json", "ShowBalance"))
-# c.run([("__ne__", z3.IntVal(0))])
+        stdev = statistics.pstdev(execution_time)
+        mean = statistics.mean(execution_time)
+        with open("results.csv", "a", newline="") as r:
+            writer = csv.writer(r, delimiter=",")
+            writer.writerow([path + method, skip_loops, mean, stdev])
